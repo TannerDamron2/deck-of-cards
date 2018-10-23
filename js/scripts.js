e9059db981ef6fd23da971d2413ea8907cf0361b
stuart gill and tanner damron <stuart@>
2018-10-23 08:09:00 -0700

    Get page that automatically generates deck of cards functioning. Issue was with triggering event. For each loops had been functional but werent showing.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-  $("#formOne").submit(function() {
+  $("button#deck").click(function(event) {
 
         var suits = ["clubs", "hearts", "spades", "diamonds"];
         var numbers = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
 
         suits.forEach(function(suit){
           numbers.forEach(function(number){
-            $("#deck").append("<li>" + number + "of" + suit + "</li>");
+            $("#allCards").append("<li>" + number + "of" + suit + "</li>");
           });
         });
 
